6a2ee92970462340f8c46fe63b05584aac123533
Andres Felipe De La Ossa Navarro <andresdelaossa@hotmail.com>
2018-12-12 18:32:56 -0500

    Initial Commit

---
+++ BitcoinTicker/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  BitcoinTicker
+//
+//  Skeleton Created by Angela Yu on 23/01/2016.
+//  Edited by Andrés Felipe De La Ossa Navarro on 12/12/2018
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        finalURL = baseURL + currencyArray[row]
+        print(currencyArray[row])
+        print(finalURL)
+    }
+    
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "COP","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    var finalURL = ""
+
+    //Pre-setup IBOutlets
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+       
+    }
+
+    
+    //TODO: Place your 3 UIPickerView delegate methods here
+    
+    
+    
+
+    
+    
+    
+//    
+//    //MARK: - Networking
+//    /***************************************************************/
+//    
+//    func getWeatherData(url: String, parameters: [String : String]) {
+//        
+//        Alamofire.request(url, method: .get, parameters: parameters)
+//            .responseJSON { response in
+//                if response.result.isSuccess {
+//
+//                    print("Sucess! Got the weather data")
+//                    let weatherJSON : JSON = JSON(response.result.value!)
+//
+//                    self.updateWeatherData(json: weatherJSON)
+//
+//                } else {
+//                    print("Error: \(String(describing: response.result.error))")
+//                    self.bitcoinPriceLabel.text = "Connection Issues"
+//                }
+//            }
+//
+//    }
+//
+//    
+//    
+//    
+//    
+//    //MARK: - JSON Parsing
+//    /***************************************************************/
+//    
+//    func updateWeatherData(json : JSON) {
+//        
+//        if let tempResult = json["main"]["temp"].double {
+//        
+//        weatherData.temperature = Int(round(tempResult!) - 273.15)
+//        weatherData.city = json["name"].stringValue
+//        weatherData.condition = json["weather"][0]["id"].intValue
+//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
+//        }
+//        
+//        updateUIWithWeatherData()
+//    }
+//    
+
+
+
+
+}
+
